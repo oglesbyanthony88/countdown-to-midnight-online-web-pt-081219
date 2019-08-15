343ca9085ae9d6bb18c4dae05a0e3cca7b583edc
Anthony Oglesby <ogle1ant@gmail.com>
2019-08-15 14:13:25 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,7 @@ def countdown(number)
   puts "#{number} SECOND(S)!"
   number +=1
 end
-
+=begin
 def countdown_with_sleep(number)
   number = 0
   while number > 10
@@ -12,3 +12,4 @@ def countdown_with_sleep(number)
   puts "#{number} SECOND(S)!"
   number +=1
 end
+=end
