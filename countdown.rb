7c23e67468e196288c5dbf45d5878eccfdbdd123
Anthony Oglesby <ogle1ant@gmail.com>
2019-08-15 14:17:35 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -13,9 +13,9 @@ puts "HAPPY NEW YEAR!"
 def countdown_with_sleep(number)
   number = 0
   while number > 10
-  sleep(1)
   puts "#{number} SECOND(S)!"
   number +=1
+  sleep(1)
 end
 puts "HAPPY NEW YEAR!"
 end
